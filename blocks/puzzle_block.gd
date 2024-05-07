93220280560d57e8c5200baa049f46c4f2c8a1d1
Chris Dosé <chris@xn--dos-dma.com>
2024-05-06 22:07:07 -0700

    Better Mario movement and animations

--- blocks/puzzle_block.gd
+++ blocks/puzzle_block.gd
@@ -12,5 +12,7 @@ func _on_hit(body: Node2D):
 				$Path2D/PathFollow2D/AnimatedSprite2D.animation = "inactive"
 				state = BlockState.INACTIVE
 				
-				var tween = get_tree().create_tween()
-				tween.tween_property($Path2D/PathFollow2D, "progress_ratio", 1.0, 0.15).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT_IN)
+				var tween = create_tween()
+				var prop_tween = tween.tween_property($Path2D/PathFollow2D, "progress_ratio", 1.0, 0.15)
+				prop_tween.set_trans(Tween.TRANS_SINE)
+				prop_tween.set_ease(Tween.EASE_OUT_IN)
--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
+const TOP_SPEED = 150.0
 const JUMP_VELOCITY = -375.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -20,9 +20,32 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction == 0:
+		velocity.x = move_toward(velocity.x, 0, 10)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, direction * TOP_SPEED, 10)
+
+	if is_on_floor():
+		if velocity.x > 0 and direction == 1:
+			$Sprite.scale.x = 1
+			$Sprite.animation = "running"
+			
+		if velocity.x < 0 and direction == -1:
+			$Sprite.scale.x = -1
+			$Sprite.animation = "running"
+			
+		if velocity.x > 0 and direction == -1:
+			$Sprite.scale.x = -1
+			$Sprite.animation = "turning"
+			
+		if velocity.x < 0 and direction == 1:
+			$Sprite.scale.x = 1
+			$Sprite.animation = "turning"
+			
+		if velocity.x == 0:
+			$Sprite.animation = "standing"
+	
+	else:
+		$Sprite.animation = "jumping"
 
 	move_and_slide()
